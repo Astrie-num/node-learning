5cdc93df2c2ba54ec6d76e072a588be239e7906b
Uwumviyimana Asterie <uwasterie07@gmail.com>
2024-08-05 10:22:32 +0200

    Learnt more modules

--- app.js
+++ app.js
@@ -8,6 +8,59 @@
 
 // modules
 
-const log = require('./logger');
+// const log = require('./logger');
 
-log("hello");
+// log("hello");
+
+
+// path module
+
+// const path = require('path');
+
+// const  pathObj = path.parse(__dirname);
+// console.log(pathObj);
+
+
+// os module
+
+
+// const os = require('os');
+
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
+
+// console.log(`Total memory : ${totalMemory}`);
+// console.log(`Free memory : ${freeMemory}`);
+
+
+
+
+// fs module
+const fs = require('fs');
+
+// synchronous method
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+
+// asynchronous method
+fs.readdir('./',(error, files) => {
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(files);
+    }
+});
+
+
+// traditional way
+
+fs.readdir('./',function(error, files){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(files);
+    }
+});
